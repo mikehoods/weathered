0f121936008cc809d824e5eee7b66f0c90255c1d
mikehoods <62457440+mikehoods@users.noreply.github.com>
2022-02-06 01:24:50 -0600

    Added icons to forecasts. Restyled.

--- src/components/SingleDayForecast.js
+++ src/components/SingleDayForecast.js
@@ -1,6 +1,16 @@
 import {useState} from 'react';
 
 import { formatDay, formatTimeString } from '../formatDateTime';
+import eye from '../assets/eye.png';
+import humidity from '../assets/humidity.png';
+import moon from '../assets/moon.png';
+import rain from '../assets/rain.png';
+import snow from '../assets/snow.png';
+import sunrise from '../assets/sunrise.png';
+import sunset from '../assets/sunset.png';
+import umbrella from '../assets/umbrella.png';
+import uvindex from '../assets/uvindex.png';
+import wind from '../assets/wind.png';
 
 export default function SingleDayForecast({ day }) {
     const [expanded, SetExpanded] = useState(false)
@@ -22,21 +32,65 @@ export default function SingleDayForecast({ day }) {
                 </div>
             </div>
             {!expanded && <div className="rainSunset_div">
-                <p className="data_p">Rain: {day.day.daily_chance_of_rain}%</p>
-                <p className="data_p">UV Index: {day.day.uv}</p>
-                <p className="data_p">Sunset: {formatTimeString(day.astro.sunset)}</p>
+                <div className="icon_div">
+                    <img className="icon" src={rain} alt="rain" />
+                    <p className="data_p">{day.day.daily_chance_of_rain}%</p>
+                </div>
+                <div className="icon_div">
+                    <img className="icon" src={sunrise} alt="sunrise" />
+                    <p className="data_p">{formatTimeString(day.astro.sunrise)}</p>
+                </div>
+                <div className="icon_div">
+                    <img className="icon" src={sunset} alt="sunrise" />
+                    <p className="data_p">{formatTimeString(day.astro.sunset)}</p>
+                </div>
             </div>}
             {expanded && <div className="expandedForecast">
-                <p className="data_p">Sunrise: {formatTimeString(day.astro.sunrise)}</p>
-                <p className="data_p">Sunset: {formatTimeString(day.astro.sunset)}</p>
-                <p className="data_p">Chance of Rain: {day.day.daily_chance_of_rain}%</p>
-                <p className="data_p">Chance of Snow: {day.day.daily_chance_of_snow}%</p>
-                <p className="data_p">Total Precipitation: {day.day.totalprecip_in}"</p>
-                <p className="data_p">Max Wind: {day.day.maxwind_mph} mph</p>
-                <p className="data_p">Humidity: {day.day.avghumidity}%</p>
-                <p className="data_p">Visibility: {day.day.avgvis_miles} miles</p>
-                <p className="data_p">UV Index: {day.day.uv}</p>
-                <p className="data_p">Moon Phase: {day.astro.moon_phase}</p>
+                <div className="col" >
+                    <div className="icon_div">
+                        <img className="icon" src={sunrise} alt="sunrise" />
+                        <p className="data_p">{formatTimeString(day.astro.sunrise)}</p>
+                    </div>
+                    
+                    <div className="icon_div">
+                        <img className="icon" src={rain} alt="rain" />
+                        <p className="data_p">{day.day.daily_chance_of_rain}%</p>
+                    </div>
+                    <div className="icon_div">
+                        <img className="icon" src={snow} alt="snow" />
+                        <p className="data_p">{day.day.daily_chance_of_snow}%</p>
+                    </div>
+                    <div className="icon_div">
+                        <img className="icon" src={umbrella} alt="umbrella" />
+                        <p className="data_p">Total Precipitation: {day.day.totalprecip_in}"</p>
+                    </div>
+                    <div className="icon_div">
+                        <img className="icon" src={humidity} alt="humidity" />
+                        <p className="data_p">Humidity: {day.day.avghumidity}%</p>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="icon_div">
+                        <img className="icon" src={sunset} alt="sunrise" />
+                        <p className="data_p">{formatTimeString(day.astro.sunset)}</p>
+                    </div>
+                    <div className="icon_div">
+                        <img className="icon" src={wind} alt="wind" />
+                        <p className="data_p">Max Wind: {day.day.maxwind_mph} mph</p>
+                    </div>
+                    <div className="icon_div">
+                        <img className="icon" src={eye} alt="eye" />
+                        <p className="data_p">Visibility: {day.day.avgvis_miles} miles</p>
+                    </div>
+                    <div className="icon_div">
+                        <img className="icon" src={uvindex} alt="uvindex" />
+                        <p className="data_p">UV Index: {day.day.uv}</p>
+                    </div>
+                    <div className="icon_div">
+                        <img className="icon" src={moon} alt="moon" />
+                        <p className="data_p">{day.astro.moon_phase}</p>
+                    </div>
+                </div>
             </div>}
         </div>
     )
